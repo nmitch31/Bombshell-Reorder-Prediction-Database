8c9d0c1cd2d28cdef29b33c51f9f9bbf95142e2b
Nicholas Mitchell <112499143+nmitch31@users.noreply.github.com>
2026-02-18 16:55:57 -0500

    Included the push to postgres part
    previously this file was just the column dropping and reorganizing and I just added the bottom chunk where we load it into Postgres

--- Clean-and-push-to-database.py
+++ Clean-and-push-to-database.py
@@ -25,3 +25,28 @@ df_clean = df[columns_to_keep].copy()
 df_clean = df_clean.rename(columns={"Id": "Order ID"})
 
 df_clean.to_csv("shopify_orders_columns_cleaned.csv", index=False)
+
+
+from sqlalchemy import create_engine
+
+# 1. Load CSV
+df = pd.read_csv(
+    r"C:\Users\thenm\Desktop\Get-a-job\shopify_orders_columns_cleaned.csv",
+    parse_dates=["Created at"]
+)
+
+# 2. Create Postgres engine
+engine = create_engine(
+    "postgresql+psycopg2://postgres:Nickanator11@localhost:5432/test_db"
+)
+
+# 3. Load into Postgres
+df.to_sql(
+    "shopify_orders_raw",
+    engine,
+    schema="analytics",
+    if_exists="replace",   # use "append" later
+    index=False
+)
+
+print("Upload complete.")
